617e7e48ae9c999855ecea77e49853c14d4b8781
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2020-02-04 17:02:59 +0300

    Add files via upload

--- Hafta1_Sec2.cs
+++ Hafta1_Sec2.cs
@@ -7,58 +7,37 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            /*
+             * İki sayının toplamını ekrana yazdıralım
+             */
             int sayi1, sayi2;
+            
             sayi1 = 1;
             Console.WriteLine(sayi1);
             sayi2 = 2;
             Console.WriteLine(sayi2);
-            //Bu iki sayının toplamını yazdıralım
-            Console.WriteLine(sayi1 + sayi2);
             Console.WriteLine("Toplam:" + sayi1 + sayi2);
             //sayi1 ve sayi2 değerlerini yan yana toplar
             //Matematiksel toplama işlemi yapmaz
-            Console.WriteLine("Toplam:" + (sayi1 + sayi2));
+
+            Console.WriteLine("Toplam:"+(sayi1 + sayi2));
             //Önce sayıları toplar, sonra toplamı yazdırır
-
-            string kelime = "Merhaba";
-            Console.WriteLine("Kelime:" + kelime);
-
-
-
+            
             /*
-             * Kullanıcının girdiği 2 sayının toplamını yazdıralım
+             * Klavyeden girilen iki sayının toplamını yazdıralım
              */
             
-            Console.Write("1.Sayı..:");
-            sayi1=Convert.ToInt16(Console.ReadLine());
-            //Console'dan okunan değerler string olarak saklanır
-            //int değere atama yaparken mutlaka convert etmemiz gerekir
-            Console.Write("2.Sayı..:");
+            Console.WriteLine("İki sayı giriniz..:");
+            sayi1 = Convert.ToInt16(Console.ReadLine());
             sayi2 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Toplam:" + (sayi1 + sayi2));
             
-            /*
-             * Klavyeden girilen sayı tek ise sayının karesini,
-             * çift ise küpünü ekrana yazdıralım
-             */
-            int sayi;
-            /*
-            Console.Write("Sayı..:");
-            sayi = Convert.ToInt16(Console.ReadLine());
-            if (sayi % 2 == 0)
-                Console.WriteLine(Math.Pow(sayi, 3));
-            else
-                Console.WriteLine(Math.Pow(sayi, 2));
-                */
-
             string isim;
-            
-            Console.Write("İsminiz...:");
+            Console.Write("İsminiz..:");
             isim = Console.ReadLine();
             Console.WriteLine("Hoşgeldin " + isim);
-            Console.WriteLine("İsminiz " + isim[0] + " harfiyle başlıyor");
-            Console.WriteLine("İsminiz " + isim[isim.Length-1] + " harfiyle bitiyor");
+            Console.WriteLine("isminiz " + isim[0] + " harfiyle başlıyor.");
+            Console.WriteLine("isminiz " + isim[isim.Length-1] + " harfiyle bitiyor.");
             
             /*
              * Kullanıcının girdiği kelimenin uzunluğuna göre;
@@ -66,19 +45,37 @@ namespace ConsoleApp1
              * 5 ile 9 arasında ise "Kelime orta uzunlukta"
              * 5 altında ise "Kelime çok kısa" çıktısı verelim
              */
+            string kelime;
             int uzunluk;
             
-            uzunluk = isim.Length;
-            if (uzunluk>=10)
+            Console.Write("Kelime..:");
+            kelime = Console.ReadLine();
+            uzunluk = kelime.Length;
+            Console.WriteLine("Kelimenin uzunluğu:" + uzunluk);
+            if (uzunluk >= 10)
                 Console.WriteLine("Kelime çok uzun");
-            else if (uzunluk>=5)
+            else if (uzunluk >= 5)
                 Console.WriteLine("Kelime orta uzunlukta");
             else
                 Console.WriteLine("Kelime çok kısa");
-            Console.WriteLine("Uzunluk:" + uzunluk);
-           
-            //Console ekranını bir tuşa basılıncaya
-            //kadar bekletir.
+            //Soru: Boşluğu dahil etmeden kelime nasıl alabilir?
+            
+
+            /*
+             * Klavyeden girilen sayı tek ise karesini,
+             * Çift ise küpünü ekrana yazdıralım
+             */
+            int sayi;
+            
+            Console.Write("Sayı..:");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            if (sayi % 2 == 0)
+                //sayı çifttir; küpünü yazdıralım
+                Console.WriteLine("Küpü:" + Math.Pow(sayi, 3));
+            else
+                //sayı tektir; karesini yazdıralım
+                Console.WriteLine("Karesi:" + Math.Pow(sayi, 2));
+            
 
             /*
              * Sayının 2,5 ve 10'a bölünebilme durumlarını yazdıralım
@@ -88,28 +85,24 @@ namespace ConsoleApp1
              * 10'a bölünebilir
              * 2,5 ve 10'a bölünemez
              */
-             
+            
             Console.Write("Sayı..:");
-            sayi =Convert.ToInt16(Console.ReadLine());
-            if (sayi%10==0)
-                Console.WriteLine("10'a bölünebilir");
-            else if (sayi%2==0)
-                Console.WriteLine("2'ye bölünebilir");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            if (sayi % 10 == 0)
+                Console.WriteLine("10'a bölünebilir.");
+            else if (sayi % 2 == 0)
+                Console.WriteLine("2'ye bölünebilir.");
             else if (sayi%5==0)
-                Console.WriteLine("5'e bölünebilir");
+                Console.WriteLine("5'ye bölünebilir.");
             else
-                Console.WriteLine("2'ye, 5'e ve 10'a bölünemez");
+                Console.WriteLine("2'ye, 5'e ve 10'a bölünemez.");
                 
-            //Soru: bölünür mü demeliyiz bölünebilir mi :)
-            //bölünebilir = istersen bölebilirsin (keyfine kalmış:)
-            //bölünebilir = başka sayılara da bölünebilir
-
             /* 
-            * 1 ile 10 arasındaki sayıları tab boşluğu bırakarak
-            * yan yana ekrana yazdıralım
-            */
+             * 1 ile 10 arasındaki sayıları tab boşluğu bırakarak
+             * yan yana ekrana yazdıralım
+             */
             int i;
-            for (i = 1 ; i <= 10; i++)
+            for (i = 1; i <= 10; i++)
                 Console.Write(i + "\t");
             Console.WriteLine();
 
@@ -117,30 +110,28 @@ namespace ConsoleApp1
              * 1 ile 10 arasındaki tek sayıları tab boşluğu bırakarak
              * yan yana ekrana yazdıralım
              */
-
-            //1.Yöntem:
+            //1.Çözüm:
             for (i = 1; i <= 10; i++)
                 if (i % 2 == 1)
                     Console.Write(i + "\t");
             Console.WriteLine();
 
-            //2.Yöntem:
-            for (i = 1; i <= 10; i+=2)
-                 Console.Write(i + "\t");
+            //2.Çözüm:
+            for (i = 1; i <= 10; i+=2) //i=i+2
+                   Console.Write(i + "\t");
             Console.WriteLine();
-
             /* 
              * 1 ile 10 arasındaki çift sayıları tab boşluğu bırakarak
              * yan yana ekrana yazdıralım
              */
-            //1.Yöntem:
+            //1.Çözüm:
             for (i = 1; i <= 10; i++)
                 if (i % 2 == 0)
                     Console.Write(i + "\t");
             Console.WriteLine();
 
-            //2.Yöntem:
-            for (i = 2; i <= 10; i += 2)
+            //2.Çözüm:
+            for (i = 2; i <= 10; i += 2) //i=i+2
                 Console.Write(i + "\t");
             Console.WriteLine();
 
@@ -148,11 +139,10 @@ namespace ConsoleApp1
              * Kullanıcının girdiği 5 değerin toplamını yazdıralım
              */
             int toplam=0;
-            for (i = 1; i < 6; i++){
-                Console.Write("Sayı..:");
+            for (i = 1 ; i < 6; i++){
+                Console.Write("Sayı...:");
                 sayi = Convert.ToInt16(Console.ReadLine());
                 toplam += sayi; //toplam = toplam + sayi;
-                //toplam += Convert.ToInt16(Console.ReadLine());
             }
             Console.WriteLine("Toplam:" + toplam);
 
@@ -167,20 +157,19 @@ namespace ConsoleApp1
             int tektoplam = 0, cifttoplam = 0;
             for (i = 1; i < 6; i++)
             {
-                Console.Write("Sayı..:");
+                Console.Write("Sayı...:");
                 sayi = Convert.ToInt16(Console.ReadLine());
                 toplam += sayi; //toplam = toplam + sayi;
-                if (sayi % 2 == 0)
+                if (i % 2 == 0)
                     cifttoplam += sayi;
                 else
                     tektoplam += sayi;
             }
             Console.WriteLine("Toplam:" + toplam);
-            Console.WriteLine("Tek Toplam:" + tektoplam);
-            Console.WriteLine("Çift Toplam:" + cifttoplam);
-
-
+            Console.WriteLine("Tek toplam:" + tektoplam);
+            Console.WriteLine("Çift toplam:" + cifttoplam);
             Console.ReadKey();
+            //Console ekranını bir tuşa basılıncaya kadar bekletir.
         }
     }
 }
