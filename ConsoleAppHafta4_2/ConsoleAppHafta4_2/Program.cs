eac0aa7b51c3021e980a52c05bc69740d0903d2c
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2020-03-13 12:01:36 +0300

    Add files via upload

---
+++ ConsoleAppHafta4_2/ConsoleAppHafta4_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppHafta4_2
+{
+    class Personel
+    {
+        public int maas;
+        public int yas;
+        public string gorev;
+        public string cinsiyet;
+        public int girisyili;
+        public int cikisyili;
+        public bool calismadurumu;
+
+        
+
+
+    }
+    class Idaripersonel : Personel
+    {
+          public string calismasaatleri; 
+    }
+    class AkademikPersonel : Personel
+    {
+        public string unvan;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Varsayılan Yapıcı Metot(Default Constructors)
+        }
+    }
+}
